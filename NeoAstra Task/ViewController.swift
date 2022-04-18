a8685f3104fb2fe6c87d3ffc798acafdd7763c38
Sai Prasanth <saiprasanthamuluru@gmail.com>
2022-04-18 16:22:14 +0530

    Initial Commit

---
+++ NeoAstra Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoAstra Task
+//
+//  Created by Sai Prasanth Amuluru on 18/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NeoAstra TaskUITests/NeoAstra_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NeoAstra_TaskUITestsLaunchTests.swift
+//  NeoAstra TaskUITests
+//
+//  Created by Sai Prasanth Amuluru on 18/04/22.
+//
+
+import XCTest
+
+class NeoAstra_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
